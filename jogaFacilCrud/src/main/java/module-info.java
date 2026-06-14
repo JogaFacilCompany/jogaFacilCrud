be5a24da99d8c2e28a95c8bfa48d5bbcab5ac507
lstsa <luis.lustosa1981@gmail.com>
2026-06-14 19:41:34 -0300

    removendo arquivos

--- jogaFacilCrud/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module br.com.jogafacil {
-    requires javafx.controls;
-    exports br.com.jogafacil;
-    exports br.com.jogafacil.util;
-    exports br.com.jogafacil.model;
-    opens br.com.jogafacil.model to javafx.base;
-    exports br.com.jogafacil.view;
-}
